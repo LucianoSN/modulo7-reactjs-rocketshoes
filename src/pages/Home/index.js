608a696bcd44a3a5127503a34be364720da95508
Luciano de Souza Nery <lsnery@gmail.com>
2019-07-24 19:34:09 -0300

    estilização da home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,101 @@
 import React from 'react';
+import { MdShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-	return <div />;
+	return (
+		<ProductList>
+			<li>
+				<img
+					src="https://static.netshoes.com.br/produtos/tenis-nike-dart-12-msl-masculino/50/D12-2683-050/D12-2683-050_detalhe1.jpg?resize=280:280"
+					alt="Tênis"
+				/>
+				<strong>Tênis muito legal</strong>
+				<span>R$129,00</span>
+
+				<button>
+					<div>
+						<MdShoppingCart size={16} color="#fff" /> 3
+					</div>
+					<span>ADICIONAR AO CARRINHO</span>
+				</button>
+			</li>
+			<li>
+				<img
+					src="https://static.netshoes.com.br/produtos/tenis-nike-dart-12-msl-masculino/50/D12-2683-050/D12-2683-050_detalhe1.jpg?resize=280:280"
+					alt="Tênis"
+				/>
+				<strong>Tênis muito legal</strong>
+				<span>R$129,00</span>
+
+				<button>
+					<div>
+						<MdShoppingCart size={16} color="#fff" /> 3
+					</div>
+					<span>ADICIONAR AO CARRINHO</span>
+				</button>
+			</li>
+			<li>
+				<img
+					src="https://static.netshoes.com.br/produtos/tenis-nike-dart-12-msl-masculino/50/D12-2683-050/D12-2683-050_detalhe1.jpg?resize=280:280"
+					alt="Tênis"
+				/>
+				<strong>Tênis muito legal</strong>
+				<span>R$129,00</span>
+
+				<button>
+					<div>
+						<MdShoppingCart size={16} color="#fff" /> 3
+					</div>
+					<span>ADICIONAR AO CARRINHO</span>
+				</button>
+			</li>
+			<li>
+				<img
+					src="https://static.netshoes.com.br/produtos/tenis-nike-dart-12-msl-masculino/50/D12-2683-050/D12-2683-050_detalhe1.jpg?resize=280:280"
+					alt="Tênis"
+				/>
+				<strong>Tênis muito legal</strong>
+				<span>R$129,00</span>
+
+				<button>
+					<div>
+						<MdShoppingCart size={16} color="#fff" /> 3
+					</div>
+					<span>ADICIONAR AO CARRINHO</span>
+				</button>
+			</li>
+			<li>
+				<img
+					src="https://static.netshoes.com.br/produtos/tenis-nike-dart-12-msl-masculino/50/D12-2683-050/D12-2683-050_detalhe1.jpg?resize=280:280"
+					alt="Tênis"
+				/>
+				<strong>Tênis muito legal</strong>
+				<span>R$129,00</span>
+
+				<button>
+					<div>
+						<MdShoppingCart size={16} color="#fff" /> 3
+					</div>
+					<span>ADICIONAR AO CARRINHO</span>
+				</button>
+			</li>
+			<li>
+				<img
+					src="https://static.netshoes.com.br/produtos/tenis-nike-dart-12-msl-masculino/50/D12-2683-050/D12-2683-050_detalhe1.jpg?resize=280:280"
+					alt="Tênis"
+				/>
+				<strong>Tênis muito legal</strong>
+				<span>R$129,00</span>
+
+				<button>
+					<div>
+						<MdShoppingCart size={16} color="#fff" /> 3
+					</div>
+					<span>ADICIONAR AO CARRINHO</span>
+				</button>
+			</li>
+		</ProductList>
+	);
 }
