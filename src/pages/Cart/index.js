94828c91952daae765ea2ed269f44a654a9358ec
Luciano de Souza Nery <lsnery@gmail.com>
2019-07-26 17:45:41 -0300

    alterando quantidades

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -10,7 +10,15 @@ import {
 import * as CartActions from '../../store/modules/cart/actions';
 import { Container, ProductTable, Total } from './styles';
 
-function Cart({ cart, removeFromCart }) {
+function Cart({ cart, removeFromCart, updateAmount }) {
+	const increment = product => {
+		updateAmount(product.id, product.amount + 1);
+	};
+
+	const decrement = product => {
+		updateAmount(product.id, product.amount - 1);
+	};
+
 	return (
 		<Container>
 			<ProductTable>
@@ -34,7 +42,10 @@ function Cart({ cart, removeFromCart }) {
 							</td>
 							<td>
 								<div>
-									<button type="button">
+									<button
+										type="button"
+										onClick={() => decrement(product)}
+									>
 										<MdRemoveCircleOutline
 											size={20}
 											color="#7159c1"
@@ -44,7 +55,10 @@ function Cart({ cart, removeFromCart }) {
 										type="number"
 										value={product.amount}
 									/>
-									<button type="button">
+									<button
+										type="button"
+										onClick={() => increment(product)}
+									>
 										<MdAddCircleOutline
 											size={20}
 											color="#7159c1"
