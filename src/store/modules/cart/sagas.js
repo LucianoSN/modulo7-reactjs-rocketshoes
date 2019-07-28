bbe8e0e440703005d7330b05d4816edf8e12f656
Luciano de Souza Nery <lsnery@gmail.com>
2019-07-27 23:07:00 -0300

    estoque na adição

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -9,8 +9,18 @@ function* addToCart({ id }) {
 		state.cart.find(p => p.id === id)
 	);
 
+	const stock = yield call(api.get, `/stock/${id}`);
+
+	const stockAmount = stock.data.amount;
+	const currentAmount = productExist ? productExist.amount : 0;
+	const amount = currentAmount + 1;
+
+	if (amount > stockAmount) {
+		console.tron.warn('ERROR');
+		return;
+	}
+
 	if (productExist) {
-		const amount = productExist.amount + 1;
 		yield put(updateAmount(id, amount));
 	} else {
 		const response = yield call(api.get, `/products/${id}`);
